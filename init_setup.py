6e8bdfdf261d84c1b58a2e5c92476804edd84717
IOT-HOME-DEV <thanasak.y@absolute.co.th>
2023-11-14 10:37:35 +0700

    init_setup

--- init_setup.py
+++ init_setup.py
@@ -7,7 +7,7 @@ def install_prereqs():
 	# os.system('apt install python3 python3-rpi.gpio python3-pip dnsmasq hostapd -y')
 	os.system('clear')
 	print("Installing paho-mqtt pyserial...")
-	print()
+	print("eieiza55+")
 	os.system('pip3 install paho-mqtt; pip3 install pyserial')
 	os.system('clear')
 
