30924aa610a79f44a952dce2e310cf731c9a8632
IOT-HOME-DEV <thanasak.y@absolute.co.th>
2023-11-14 10:23:48 +0700

    edit init_setup

--- init_setup.py
+++ init_setup.py
@@ -6,8 +6,8 @@ def install_prereqs():
 	os.system('clear')
 	# os.system('apt install python3 python3-rpi.gpio python3-pip dnsmasq hostapd -y')
 	os.system('clear')
-	print("Installing Flask web server...")
+	print("Installing paho-mqtt pyserial...")
 	print()
 	os.system('pip3 install paho-mqtt; pip3 install pyserial')
-	print("Installing paho-mqtt pyserial...")
+	
 	os.system('clear')
