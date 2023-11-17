2372916748743bdccefb6361c7dc6ff4afcef5cd
IOT-HOME-DEV <thanasak.y@absolute.co.th>
2023-11-17 14:29:27 +0700

    edit reconnect mqtt and senddata

--- libs/mqtt/mqtt_eiei.py
+++ libs/mqtt/mqtt_eiei.py
@@ -6,7 +6,7 @@ from paho.mqtt import client as mqtt_client
 import random
 import time
 
-broker = 'iot-a916.local'
+broker = 'localhost'
 port = 1883
 topic = "python/mqtt"
 # Generate a Client ID with the subscribe prefix.
@@ -15,23 +15,38 @@ username = 'user'
 password = 'passwd'
 client = mqtt_client.Client(client_id)
 
+def init_mqtt():
+    client = connect_mqtt()
+    client.loop_start()
+    client.loop_stop()
+    client.disconnect()
+
 def connect_mqtt():
     def on_connect(client, userdata, flags, rc):
         print("Connected to broker")
         
         if rc == 0:
             print("Connected to MQTT Broker!")
+
         else:
             print("Failed to connect, return code %d\n", rc)
     # client = mqtt_client.Client(client_id)
     client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
+    client.publish("usb2ttl/msg", "start")
     return client
 
 
 def run_mqtt():
-    client = connect_mqtt()
-    client.loop_start()
-    # publish(client)
-    client.loop_stop()
+    try:
+        # Check if the client is still connected; if not, it will attempt to reconnect
+        if not client.is_connected():
+            print("Client is not connected. Attempting to reconnect...")
+            client.reconnect()
+        # Perform other tasks here if needed
+        time.sleep(1)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print("eiei")
--- libs/usb2serial/usb2ttl_eiei.py
+++ libs/usb2serial/usb2ttl_eiei.py
@@ -1,8 +1,10 @@
 import serial
 from libs.mqtt import mqtt_eiei 
 import json
+import random
+import time
 # Define the serial port and baud rate
-serial_port = '/dev/cu.usbserial-130'   # Change this to your 
+serial_port = '/dev/ttyUSB0'   # Change this to your 
 baud_rate = 9600  # Set the baud rate used by your ESP32
 
 # Initialize the serial connection
@@ -11,9 +13,10 @@ ser = serial.Serial(serial_port, baud_rate)
 
 #@use while true to get value
 def init_read_serial():
-     mqtt_eiei.run_mqtt()
+     mqtt_eiei.init_mqtt()
 
 def read_serial():
+    mqtt_eiei.run_mqtt()
     try:
         try:
             line = ser.readline()
@@ -26,6 +29,7 @@ def read_serial():
         else:
             print("Received: ", line)
             callback_value(line)
+            
             
     except KeyboardInterrupt:
         # Close the serial port when the program is interrupted
